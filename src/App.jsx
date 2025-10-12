94b9d20c98feb9b6a9d15b4b9563c3ad1478cb7c
Nassim Lahoudi <97837009+Nassim-Lahoudi@users.noreply.github.com>
2025-10-12 21:20:48 +0000

    Initialize maintenance page with React and Vite setup
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added App.css for styling the maintenance page layout and text.
    - Implemented App.jsx as the main component displaying a headline and a message about the website being under construction.
    - Set up main.jsx for rendering the App component within a StrictMode.
    - Configured vite.config.js for Vite with React plugin.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="page-container">
+            <div className="page-content">
+                <h1 className="headline">Nassim Lahoudi</h1>
+                <p className="text">Die Webseite ist im Aufbau, <br /> sie wird in Kürze Online sein.</p>
+            </div>
+        </div>
+    )
+}
+
+export default App
